d3c5dc798f963da149ce34e7745a2a12f0cd738e
kay <mine@local.de>
2017-06-10 16:12:19 +0200

    fixing style problems on Heroku

--- CreateInput/settings.py
+++ CreateInput/settings.py
@@ -136,15 +136,22 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 #STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'CreateInput', 'static', 'static_root')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'CreateInput', 'static', 'static_root')
 
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'CreateInput', 'static', 'static_dirs'),
+#)
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'CreateInput', 'static', 'static_dirs'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 #STATIC_ROOT = ( os.path.join(PROJECT_ROOT, "static"), )
